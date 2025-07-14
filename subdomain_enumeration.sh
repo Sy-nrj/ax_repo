0d58046ade990493c56c8b1c27dd3c0d651ad65b
Sy-nrj <lsmkark99@gmail.com>
2025-07-14 23:15:19 +0200

    Create_ subdomain_enumeration.sh

--- subdomain_enumeration.sh
+++ subdomain_enumeration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exit on error
-set -e
+set -euo pipefail
 
 # Check usage
 if [ -z "$1" ]; then
@@ -11,10 +11,14 @@ fi
 
 # Input file with domains
 domains_file="$1"
+scan_id="$2"
 
 # Timestamp for output directory
-timestamp=$(date +%Y%m%d_%H%M%S)
-base_scan_dir=~/scan/domains_$timestamp
+#timestamp=$(date +%Y%m%d_%H%M%S)
+
+base_scan_dir=~/scan/list_$scan_id
+
+
 
 echo "[*] Results will be saved in: $base_scan_dir"
 
@@ -34,17 +38,20 @@ while IFS= read -r domain || [[ -n "$domain" ]]; do
     # Subdomain Enumeration
     echo "  - Running subfinder..."
     subfinder -d "$domain" -silent -o "$recon_dir/subfinder.txt"
+    echo "[+] subfinder found $(wc -l < "$recon_dir/subfinder.txt domains")"
 
     echo "  - Running assetfinder..."
     assetfinder --subs-only "$domain" | tee "$recon_dir/assetfinder.txt" > /dev/null
+    echo "[+] assetfinder found $(wc -l | "$recon_dir/assetfinder.txt domains")"
 
     echo "  - Running findomain..."
     # echo "$domain" > "$recon_dir/tmp_domain.txt"
     findomain -t $domain --output "$recon_dir/findomain.txt"
+    echo "[+] Findomain found $(wc -l < "$recon_dir/findomain.txt domains") "
 
     # Combine results
     cat "$recon_dir"/*.txt | sort -u > "$recon_dir/all_domains.txt"
-    echo "  [+] Total unique subdomains found: $(wc -l < "$recon_dir/all.txt")"
+    echo "  [+] Total unique subdomains found: $(wc -l < "$recon_dir/all_subs.txt")"
 
 done < "$domains_file"
 
